70f50f1050ca373b6df8bb945485dfd86a13ec1f
Vinicius Hoffmann <vinicius.hoffmann.oliveira@gmail.com>
2026-06-30 23:25:54 -0300

    feat(geografia): create high-performance ReadOnlyModelViewSet for Cidade

--- geografia/views.py
+++ geografia/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Cidade
+from .serializers import CidadeSerializer
+
+class CidadeViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint que permite visualizar as cidades cadastradas no ERP.
+    """
+    queryset = Cidade.objects.select_related('cod_est').all().order_by('descricao')
+    serializer_class = CidadeSerializer
+    permission_classes = [IsAuthenticated]  # Protege o endpoint pedindo autenticação
