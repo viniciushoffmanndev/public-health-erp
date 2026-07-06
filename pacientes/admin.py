946d6b95631e5ff8b4e40b6430c93901b6f36816
Vinicius Oliveira Hoffmann de C Pais <vinicius.hoffmann.oliveira@gmail.com>
2026-07-06 16:20:02 -0300

    feat(pacientes): register models in admin and optimize performance using raw_id_fields

--- pacientes/admin.py
+++ pacientes/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import UsuarioCadsus, Pessoa
+
+@admin.register(UsuarioCadsus)
+class UsuarioCadsusAdmin(admin.ModelAdmin):
+    # Exibe dados capitais e limpos para triagem e busca na listagem
+    list_display = ('cd_usu_cadsus', 'nm_usuario', 'cpf', 'dt_nascimento', 'prontuario', 'st_vivo')
+    search_fields = ('nm_usuario', 'cpf', 'prontuario')
+    list_filter = ('sg_sexo', 'st_vivo', 'st_excluido')
+    ordering = ('cd_usu_cadsus',)
+
+    # Transforma dropdowns em inputs de busca numéricos para poupar memória do servidor
+    raw_id_fields = (
+        'cod_cid_nascimento', 'cd_municipio_residencia', 'cd_cbo', 
+        'cd_escolaridade', 'cd_usuario', 'cd_usuario_cad',
+        'cd_pais_nascimento', 'cd_raca', 'cd_estado_civil',
+        'empresa_responsavel', 'cd_local_permanencia', 'nr_atendimento_origem',
+        'cd_endereco', 'cd_usu_cadsus_responsavel', 'cd_etnia',
+        'cd_gerenciador_arquivo', 'cd_estabelecimento_cerest', 'cd_usu_cadsus_unificado',
+        'cd_equipe', 'cd_equipe_profissional', 'cd_comunidade',
+        'cd_motivo_cpf', 'cd_equipe_vinculo'
+    )
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('cod_pessoa', 'descricao', 'cnpj_cpf', 'fis_jur', 'dt_cadastro')
+    search_fields = ('descricao', 'cnpj_cpf')
+    list_filter = ('fis_jur', 'cliente', 'fornecedor', 'funcionario')
+    ordering = ('cod_pessoa',)
+    
+    # Protege a memória contra dropdowns pesados nas chaves estrangeiras
+    raw_id_fields = ('cod_atv', 'cod_tip_pessoa', 'cod_representante')
